cd9be31af69df159d09269530fe3e2b94a26e01b
Berkay YAY <berkayyay7@gmail.com>
2023-02-13 13:55:23 +0300

    Login Screen added. .gitignore file modified.

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Berkay YAY on 11.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = LoginViewController()
+        
+        return true
+    }
+}
+
