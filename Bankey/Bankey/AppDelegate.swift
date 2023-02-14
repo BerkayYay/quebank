6c731ecf4f4b61b0dea86fd77340592f2267b75a
Berkay YAY <berkayyay7@gmail.com>
2023-02-14 14:45:51 +0300

    First Onboarding Screen added to OnboardingViewContainer.

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = OnboardingContainerVC()
+//        window?.rootViewController = OnboardingContainerVC()
+        window?.rootViewController = OnboardingVC()
         
         
         return true
---
+++ Bankey/Bankey/OnboardingVC.swift
@@ -0,0 +1,60 @@
+//
+//  OnboardingViewController.swift
+//  Bankey
+//
+//  Created by Berkay YAY on 14.02.2023.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+    
+    let stackView = UIStackView()
+    let label = UILabel()
+    let imageView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+
+extension OnboardingVC {
+    
+    func style(){
+        // StackView
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        // Image
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "delorean")
+        
+        // Label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = "Quebank is faster, easier to use and has a brand new look and fell that will make you feel like you are back in 1989."
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+    }
+    
+    func layout(){
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
+
